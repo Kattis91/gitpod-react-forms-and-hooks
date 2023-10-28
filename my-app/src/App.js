99adff5c766f7c6dec0fda73bc50810566f79577
Kattis91 <katya.svedmark@gmail.com>
2023-10-28 09:40:31 +0000

    Move files fromm nother workspace

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+// import ControlledFormHooks from './components/ControlledFormHooks';
+// import ControlledForms from './components/ControlledForms';
+// import UncontrolledForm from './components/UncontrolledForm';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithObjects from './components/UseStateWithObjects';
+// import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <ControlledFormHooks /> */}
+            {/* <ControlledForms /> */}
+            {/* <UncontrolledForm /> */}
+            {/* <UseStateWithArrays /> */}
+            {/* <UseStateWithObjects /> */}
+            {/* <UseEffectCounter /> */}
+            <UseEffectCounterContainer />
+        </div>
+    );
+}
+
+export default App;
