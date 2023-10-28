c14965ce6df30aea1fbfdf169a308c9ba20770c1
Kattis91 <katya.svedmark@gmail.com>
2023-10-28 15:52:36 +0000

    Fetch Data with GET

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HTTPRequest from './components/HTTPRequest';
 // import ControlledFormHooks from './components/ControlledFormHooks';
 // import ControlledForms from './components/ControlledForms';
 // import UncontrolledForm from './components/UncontrolledForm';
 // import UseStateWithArrays from './components/UseStateWithArrays';
 // import UseStateWithObjects from './components/UseStateWithObjects';
 // import UseEffectCounter from './components/UseEffectCounter';
-import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+// import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 function App() {
     return (
@@ -17,7 +18,8 @@ function App() {
             {/* <UseStateWithArrays /> */}
             {/* <UseStateWithObjects /> */}
             {/* <UseEffectCounter /> */}
-            <UseEffectCounterContainer />
+            {/* <UseEffectCounterContainer /> */}
+            <HTTPRequest />
         </div>
     );
 }
